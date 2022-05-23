159ec99a4db2162772b4fcc40f811df64688d196
Guilherme Podolak <podolak12345@gmail.com>
2022-05-23 16:16:01 -0300

    Requisito 1 getElementsById

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (!ids) {
+    return [];
+  }
+  const getSpecieId = data.species.filter((specieId) => ids.includes(specieId.id));
+  return getSpecieId;
 }
 
 module.exports = getSpeciesByIds;
