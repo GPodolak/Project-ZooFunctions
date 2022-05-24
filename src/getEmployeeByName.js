2bfddb295fe364598d8cd501eac7a76fcea308d6
Guilherme Podolak <podolak12345@gmail.com>
2022-05-24 11:43:43 -0300

    requisito 3 GetEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) return {};
+  const checkName = data.employees.filter(
+    (nameObject) => nameObject.firstName === employeeName || nameObject.lastName === employeeName,
+  );
+  return checkName[0];
 }
 
 module.exports = getEmployeeByName;
