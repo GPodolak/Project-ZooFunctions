bd0066e7b4d24e982b927043baf7e58437b9b4b6
Guilherme Podolak <podolak12345@gmail.com>
2022-05-23 22:08:21 -0300

    requisito 2 getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalNames = data.species.find(({ name }) => name === animal);
+  const animalAge = animalNames.residents.every((age1) => age1.age >= age);
+  return animalAge;
 }
 
 module.exports = getAnimalsOlderThan;
