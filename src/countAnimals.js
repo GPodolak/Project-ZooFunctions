8b467eec37b54e187ad7759e66e7a311527d748c
Guilherme Podolak <podolak12345@gmail.com>
2022-05-25 22:46:05 -0300

    requisito 5 countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 // const { species } = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
+const getAnimalName = (animal) => species.find((specie) => specie.name === animal.specie);
+const getAnimalSex = (choosenAnimal, animal) => {
+  const sexAnimal = choosenAnimal.residents.filter((resident) => resident.sex === animal.sex);
+  return sexAnimal.length;
+};
 function countAnimals(animal) {
   // seu código aqui
-
+  const animalType = {};
+  if (!animal) {
+    species.forEach(({ name, residents }) => {
+      animalType[name] = residents.length;
+    }); return animalType;
+  }
+  const choosenAnimal = getAnimalName(animal);
+  if (animal.sex) {
+    return getAnimalSex(choosenAnimal, animal);
+  }
+  return choosenAnimal.residents.length;
 }
 module.exports = countAnimals;
