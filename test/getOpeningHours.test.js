722afb8212f4aed66d82431f4bbfc26904652206
Guilherme Podolak <podolak12345@gmail.com>
2022-05-31 13:24:11 -0300

    requisito 7 handlerElephants

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,9 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () =>
+it('weekDays');
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+expect(days).toBe(weekDays);
+
+
+});
