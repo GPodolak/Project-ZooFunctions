3e88a9404f9a609565484efe0926ab6a3fa0f707
Guilherme Podolak <podolak12345@gmail.com>
2022-05-31 14:31:13 -0300

    requisito 7 90%handler

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -23,4 +23,7 @@ describe('Testes da função HandlerElephants', () => {
   it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability')).toEqual(expect.not.arrayContaining(['Monday']));
   });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(undefined).toBe(handlerElephants());
+  });
 });
